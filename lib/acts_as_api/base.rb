423e58a935166c74644719e7f57a66cc1f252d9e
Dmitry Ribalka <dmitry.ribalka@gmail.com>
2015-08-03 13:00:25 +0300

    fix crashes

--- lib/acts_as_api/base.rb
+++ lib/acts_as_api/base.rb
@@ -60,15 +60,13 @@ module ActsAsApi
         api_attributes = self.class.api_accessible_attributes(api_template)
         raise ActsAsApi::TemplateNotFoundError.new("acts_as_api template :#{api_template.to_s} was not found for model #{self.class}") if api_attributes.nil?
 
-        if self.methods(:before_api_response).arity == 1
-          Rails.logger.warning "[ActsAsApi] callback before_api_response with one argument is DEPRECATED"
+        if self.method(:before_api_response).arity == 1
           before_api_response(api_template)
         else
           before_api_response(api_template, options)
         end
 
-        response_hash = if self.methods(:around_api_response).arity == 1
-                          Rails.logger.warning "[ActsAsApi] callback around_api_response with one argument is DEPRECATED"
+        response_hash = if self.method(:around_api_response).arity == 1
                           around_api_response(api_template) do
                             api_attributes.to_response_hash(self, api_attributes)
                           end
@@ -78,8 +76,7 @@ module ActsAsApi
                           end
                         end
 
-        if self.methods(:after_api_response).arity == 1
-          Rails.logger.warning "[ActsAsApi] callback after_api_response with one argument is DEPRECATED"
+        if self.method(:after_api_response).arity == 1
           after_api_response(api_template)
         else
           after_api_response(api_template, options)
